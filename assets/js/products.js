22736634c3b21630709dda058fa6ccaf83283ad8
Alexandru Pietrareanu <alex.pietrareanu@sameday.ro>
2023-06-29 16:57:32 +0300

    june_29

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
+import App from '@/pages/products';
 
-const app = new Vue({
-    el: '#app',
-    template: '<h1>hello vue! are you with me? boye</h1>',
-});
+new Vue({
+    render: (h) => h(App),
+}).$mount('#app');
